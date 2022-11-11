741074d6e918af75110ad44b92e1a9e106d95083
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-11-11 12:11:54 -0500

    Renewed repo files

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -1,7 +1,7 @@
-"""portfolio URL Configuration
+"""personal_portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,12 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-# statics files are normally hosted in the cloud i.e. aws
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- projects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.project_index, name='projects_list'),
-    # path('projects/<int:pk>', views.project_detail, name='projects_detail')
-]
