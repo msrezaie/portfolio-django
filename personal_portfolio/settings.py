d1bcbf702cf16410178c39f9e444b457288e966d
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-11-11 21:52:34 -0500

    Added 'about me' and 'projects' sections

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = 'staticfiles/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
