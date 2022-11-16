7e82fb17dfa4252f211efc88453196c0fea10a11
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-11-16 17:22:11 -0500

    Project model created

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    desc = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # unique attribute meaning: no other value can have
+    # the same value as the value of the following 'id'
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
--- base/views.py
+++ base/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+ 
 
 def home(request):
-    return render(request, 'base\home.html')
+    msg = "hello you are on the projects section"
+    return render(request, 'base\home.html', {'msg': msg})
+
+def project_detail(request, pk):
+    msg = "hello you are on the home page"
+    return render(request, 'base\home.html', {'msg': msg})
