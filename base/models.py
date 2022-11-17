36d0e444dd5068d92d30479414a67cce4641b1c1
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-11-17 13:46:24 -0500

    Added a Tag model

--- base/models.py
+++ base/models.py
@@ -7,6 +7,7 @@ class Project(models.Model):
     desc = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     # unique attribute meaning: no other value can have
     # the same value as the value of the following 'id'
@@ -15,3 +16,13 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
