5e821ccbd86b54238f9a1c9ed49f42994531071f
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-11-19 18:35:37 -0500

    Added a model for the profile

--- base/models.py
+++ base/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null= True, blank=True)
+    name = models.CharField(max_length=200, null= True, blank=True)
+    email = models.EmailField(max_length=200, null= True, blank=True)
+    short_bio = models.TextField(null= True, blank=True)
+    long_bio = models.CharField(max_length=200, null= True, blank=True)
+    profile_image = models.ImageField(null=True, blank=True, default="no-image.jpg")
+    social_github = models.CharField(max_length=200, null= True, blank=True)
+    social_linkedin = models.CharField(max_length=200, null= True, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.user.username)
 
 
 class Project(models.Model):
