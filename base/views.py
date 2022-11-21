9663bd0eca247f7dfe4829fc09f8d2edf7bac3d3
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2022-11-21 14:14:36 -0500

    Restructured the home page

--- base/views.py
+++ base/views.py
@@ -4,8 +4,10 @@ from . models import Project, Profile
 
 def home(request):
     projects = Project.objects.all()
-    profile = Profile.objects.all()
-    context = {'projects': projects, 'profile': profile}
+    profile = Profile.objects.first()
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+    context = {'projects': projects, 'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
     return render(request, 'base\home.html', context)
 
 def project_detail(request, pk):
